5a983942b85ce3db0e5b9e45c98c5a2a22617273
dbsalsgur <rkawkaos123@naver.com>
2023-08-16 02:08:00 +0900

    사이퍼즈 api 기본 틀 작성
    내부적 제한이나 파라미터 다시 확인 후 수정예정

--- src/main/java/com/cyphers/game/RecordSearch/cyphers/CyphersApiService.java
+++ src/main/java/com/cyphers/game/RecordSearch/cyphers/CyphersApiService.java
@@ -17,9 +17,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.cyphers.game.RecordSearch.cyphers.annotation.Required;
-import com.cyphers.game.RecordSearch.cyphers.model.CyphersMatchingInfo;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersCharacterRanking;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersItemDetailInfo;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersItemSearch;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersMatchingDetails;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersMatchingHistory;
 import com.cyphers.game.RecordSearch.cyphers.model.CyphersPlayerInfo;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersPlayerRanking;
 import com.cyphers.game.RecordSearch.cyphers.model.CyphersPlayerResponse;
+import com.cyphers.game.RecordSearch.cyphers.model.CyphersTsjRanking;
 import com.cyphers.game.RecordSearch.cyphers.model.enumuration.CyphersWordType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -55,11 +61,17 @@ public class CyphersApiService {
         return EntityUtils.toString(entity);
     }
 
-    private static final String testPlayerId = "b74c1979fbc7432102a6e524ba9a467b";
+    private static final String testPlayerId = "27717b771c01560f2b64a31aecc5dbcf";
     private static final String testNickname = "땅팜";
     private static final String imageUrl = "https://img-api.neople.co.kr/cy/characters/<characterId>?zoom=<zoom>";
     private static final String testMatchId = "2edeaec3bf1f85dbc2dd97a439980bbee50165f7a05c537c310a49b545994b01";
     private static final String testRankingType = "winCount";
+    private static final String testCharacterId = "6d7db858c4f20adcf4fc2eee21c2c03f";
+    private static final String testTsjType = "melee";
+    private static final String testItemName = "파이어 포르테";
+    private static final String testWordType = "full";
+    private static final Integer testLimit = 1000;
+    private static final String testItemId = "19f0134c20a835546c760c38293ce67a";
     
 
     public static void main(String[] args) throws Exception {
@@ -67,11 +79,15 @@ public class CyphersApiService {
         params.put("nickname", testNickname);
         params.put("matchId", testMatchId);
         params.put("playerId", testPlayerId);
+        params.put("itemName", testItemName);
+        params.put("wordType", testWordType);
+        params.put("limit", testLimit.toString());
+        
         String cyPlayer = getTest("/cy/players", params);
 //        System.out.println("cyPlayer = " + cyPlayer);
-
-        String cyCharacters = getTest("/cy/characters", params);
-//        System.out.println("cyCharacters = " + cyCharacters);
+        
+        String cyPlayerInfo = getTest("/cy/players/" + testPlayerId, params);
+//        System.out.println("cyPlayerInfo = " + cyPlayerInfo);
 
         String cyMatchingHistory = getTest("/cy/players/" + testPlayerId + "/matches", params);
 //        System.out.println("cyMatchingHistory = " + cyMatchingHistory);
@@ -82,11 +98,21 @@ public class CyphersApiService {
         String cyPlayerRanking = getTest("/cy/ranking/ratingPoint", params);
 //        System.out.println("cyPlayerRanking = " + cyPlayerRanking);
         
-        String cyCharacterRanking = getTest("/cy/ranking/characters/" + "testRankingType", params);
-        System.out.println("cyCharacterRanking = " + cyCharacters);
+        String cyCharacterRanking = getTest("/cy/ranking/characters/" + testCharacterId +"/" + testRankingType, params);
+//        System.out.println("cyCharacterRanking = " + cyCharacterRanking);
         
-//        String cyCharacters = getTest("/cy/characters", params);
+        String cyTsjRanking = getTest("/cy/ranking/tsj/" + testTsjType , params);
+//        System.out.println("cyTsjRanking = " + cyTsjRanking);
+        
+        String cyItemSearch = getTest("/cy/battleitems", params);
+//        System.out.println("cyItemSearch = " + cyItemSearch);
+        
+        String cyItemDetail = getTest("/cy/battleitems/" + testItemId, params);
+//        System.out.println("cyItemDetail = " + cyItemDetail);
+        
+        String cyCharacters = getTest("/cy/characters", params);
 //        System.out.println("cyCharacters = " + cyCharacters);
+        
     }
 
     // get요청 보내기
@@ -104,7 +130,8 @@ public class CyphersApiService {
         HttpEntity entity = response.getEntity();
         return EntityUtils.toString(entity);
     }
-
+    
+    //플레이어 검색
     public CyphersPlayerResponse searchPlayers(@Required String nickname, CyphersWordType wordType, Integer limit) throws Exception {
         Map<String, String> params = new HashMap<>();
         params.put("nickname", nickname);
@@ -126,29 +153,66 @@ public class CyphersApiService {
         return objectMapper.readValue(get("/cy/players", params), CyphersPlayerResponse.class);
     }
 
+    //플레이어 정보 조회
     public CyphersPlayerInfo searchPlayerInfo(@Required String playerId) throws Exception {
         Map<String, String> params = new HashMap<>();
+        params.put("playerId", playerId);
         return objectMapper.readValue(get("/cy/players/" + playerId, params), CyphersPlayerInfo.class);
     }
 
-    public CyphersPlayerInfo searchMatchingHistory(@Required String playerId) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        return objectMapper.readValue(getTest("/cy/players/" + playerId + "/matches", params), CyphersPlayerInfo.class);
-    }
-    
-    public CyphersMatchingInfo searchMatchingDetail(@Required String matchId) throws Exception {
-        Map<String, String> params = new HashMap<>();
-        return objectMapper.readValue(getTest("/cy/matches/" + matchId, params), CyphersMatchingInfo.class);
-    }
-    
-    //playerId가 있으면 해당 플레이어의 랭킹, 없으면 1위부터 limit까지 나열
-    public CyphersPlayerInfo searchPlayerRanking(@Required String playerId) throws Exception {
+    //플레이어 매칭 기록 조회
+    public CyphersMatchingHistory searchMatchingHistory(@Required String playerId) throws Exception {
         Map<String, String> params = new HashMap<>();
         params.put("playerId", playerId);
-        return objectMapper.readValue(getTest("/cy/ranking/ratingPoint", params), CyphersPlayerInfo.class);
+        return objectMapper.readValue(getTest("/cy/players/" + playerId + "/matches", params), CyphersMatchingHistory.class);
     }
-    public CyphersPlayerInfo searchRanking() throws Exception {
+    
+    //매칭 상세정보 조회
+    public CyphersMatchingDetails searchMatchingDetail(@Required String matchId) throws Exception {
         Map<String, String> params = new HashMap<>();
-        return objectMapper.readValue(getTest("/cy/ranking/ratingPoint", params), CyphersPlayerInfo.class);
+        return objectMapper.readValue(getTest("/cy/matches/" + matchId, params), CyphersMatchingDetails.class);
     }
+    
+    //통합 랭킹 조회
+    //playerId가 있으면 해당 플레이어의 랭킹, 없으면 1위부터 limit까지 나열
+    public CyphersPlayerRanking searchPlayerRanking(@Required String playerId) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("playerId", playerId);
+        return objectMapper.readValue(getTest("/cy/ranking/ratingPoint", params), CyphersPlayerRanking.class);
+    }
+    public CyphersPlayerRanking searchRanking() throws Exception {
+        Map<String, String> params = new HashMap<>();
+        return objectMapper.readValue(getTest("/cy/ranking/ratingPoint", params), CyphersPlayerRanking.class);
+    }
+    
+    //캐릭터 랭킹 조회
+    public CyphersCharacterRanking searchCharacterRanking(@Required String characterId, @Required String rankingType, String playerId) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("playerId", playerId);
+        params.put("rankingType", rankingType);
+        return objectMapper.readValue(getTest("/cy/ranking/characters/" + characterId +"/" + rankingType, params), CyphersCharacterRanking.class);
+    }
+    
+    //투신전 랭킹 조회
+    public CyphersTsjRanking searchTsjRanking(@Required String playerId, String tsjType) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("playerId", playerId);
+        return objectMapper.readValue(getTest("/cy/ranking/tsj/" + tsjType , params), CyphersTsjRanking.class);
+    }
+    
+    //아이템 검색
+    public CyphersItemSearch searchItem(@Required String itemName, String wordType, Integer limit) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        params.put("itemName", itemName);
+        params.put("wordType", wordType);
+        params.put("limit", limit.toString());
+        return objectMapper.readValue(getTest("/cy/battleitems", params), CyphersItemSearch.class);
+    }
+    
+    //아이템 상세 정보 조회
+    public CyphersItemDetailInfo searchItemDetail(@Required String itemId) throws Exception {
+        Map<String, String> params = new HashMap<>();
+        return objectMapper.readValue(getTest("/cy/battleitems/" + itemId, params), CyphersItemDetailInfo.class);
+    }
+    
 }
