78b5b027ee99909aa6cf757f6aa4d9f07f50a694
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-08-23 02:11:18 +0900

    Restdocs를 아직 추가하지 않아서 삭제처리

--- src/test/java/com/cyphers/game/RecordSearch/controller/UserControllerTest.java
+++ src/test/java/com/cyphers/game/RecordSearch/controller/UserControllerTest.java
@@ -4,7 +4,5 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
-@SpringBootTest(classes = Application.class)
 public class UserControllerTest {
 }
